060dd65b3422956ea5656a9ab408a37d364764d4
qiandeng <qiandeng@qiandeng-hz1.cisco.com>
2013-04-15 14:22:55 +0800

    Initial(Based on jwebap)

---
+++ src/main/java/org/jwebap/core/RegisterException.java
@@ -0,0 +1,20 @@
+package org.jwebap.core;
+
+
+/**
+ * Component×é¼þ×¢²áÒì³£
+ * 
+ * @author leadyu
+ * @since Jwebap 0.5
+ * @date  Aug 7, 2007
+ */
+public class RegisterException extends Exception{
+	
+	public RegisterException(String msg){
+		super(msg);
+	}
+	
+	public RegisterException(String msg,Throwable cause){
+		super(msg,cause);
+	}
+}
